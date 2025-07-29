3f8538d29022046f36d8c833750e77c7ba22f658
Alireza_Nami <nami.elec@gmail.com>
2025-07-29 22:33:53 +0330

    verify UltrasonicSensor class functionality with SRF04 on Arduino Uno

--- Software/UltraSonic_SRF04/src/main.cpp
+++ Software/UltraSonic_SRF04/src/main.cpp
@@ -1,12 +1,28 @@
 #include <Arduino.h>
+#include"ultrasonic_sensor.hpp"
 
+#define TRIG_PIN 6
+#define ECHO_PIN 7
 
+UltrasonicSensor sensor(TRIG_PIN, ECHO_PIN);
 
 void setup() {
-
+    Serial.begin(9600);
+    sensor.begin();
 }
 
 void loop() {
 
+  float distance = sensor.measureDistance(Unit::CENTIMETER);
+
+  if (distance > 0) {
+      Serial.print("Distance: ");
+      Serial.print(distance);
+      Serial.println(" cm");
+  } else {
+      Serial.println("Measurement failed");
+  }
+
+  delay(500);
 }
 
